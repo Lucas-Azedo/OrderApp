98cfe8eeeb65dbd72c57160e70800f36e6a31af1
Lucas-Azedo <2007.lucas.azevedo@gmail.com>
2025-07-08 14:42:26 -0300

    feat: h2 db for tests

--- back/OrderApp/src/test/java/com/OrderApp/OrderAppApplicationTests.java
+++ back/OrderApp/src/test/java/com/OrderApp/OrderAppApplicationTests.java
@@ -2,8 +2,10 @@ package com.OrderApp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class OrderAppApplicationTests {
 
 	@Test
